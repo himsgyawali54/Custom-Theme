211033d71c0075305ca6eaf48d7ad3810d0922ce
Paja6544 <Pavankumar8011@gmail.com>
2025-04-04 13:22:39 +0545

    other pages hero part done

--- page.php
+++ page.php
@@ -2,25 +2,29 @@
 get_header();
 the_post();
 ?>
-<div class="container">
-    <div class="row other-page-hero">
-        <div class="col-12">
+<div class="container-full other-page-hero">
+  <div class="container ">
+    <div class="row ">
+        <div class="col-12 d-flex flex-column justify-content-center align-items-center">
         <h1><?php the_title(); ?></h1>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="/">Home</a></li>
-              <li class="breadcrumb-item" aria-current="page"><?php the_title(); ?></li>
+              <li class="breadcrumb-item breadcrumb-title" aria-current="page"><?php the_title(); ?></li>
             </ol>
           </nav>
         </div>
     </div>
+    </div>
+    </div>
+    <div class="container">
     <div class="row page-content">
     <?php the_content(); ?>
+    </div>
     </div>
 
 
 
-</div>
 <?php 
 get_footer();
 ?>
