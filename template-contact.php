668d310ca598b3acdfb813309df2ab3adbc658e3
Paja6544 <Pavankumar8011@gmail.com>
2025-04-11 12:55:32 +0545

    added custom contact us page template

---
+++ template-contact.php
@@ -0,0 +1,45 @@
+<?php 
+//Template Name: Contact us
+ get_header();
+ ?>
+<section id="contact">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Contact</h2>
+          </div>
+          <div
+            class="row row-cols-1 row-cols-md-2 row-cols-lg-4 text-center contact-medium"
+          >
+            <div class="col">
+              <a href="tel:+19786376362" target="_blank">
+                <i class="bi bi-telephone"></i>
+              </a>
+              <h4>Call</h4>
+              <p>(978) 637-6362</p>
+            </div>
+            <div class="col">
+              <i class="bi bi-geo-alt"></i>
+              <h4>Address</h4>
+              <p>
+                Paradise Ridge Dr,<br />
+                Round Rock 78665, Texas
+              </p>
+            </div>
+            <div class="col">
+              <a href="mailto:info@seostairs.com">
+                <i class="bi bi-envelope"></i>
+              </a>
+              <h4>Email</h4>
+              <p>info@seostairs.com</p>
+            </div>
+            <div class="col">
+              <i class="bi bi-clock"></i>
+              <h4>Open Hours</h4>
+              <p>Mon-Fri: 7AM - 5PM <br />Sat-Sun: Closed</p>
+            </div>
+          </div>
+        </div>
+      </section>
+ <?php
+ get_footer();
+ ?>
