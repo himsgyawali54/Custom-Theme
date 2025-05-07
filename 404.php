931a6c9b2ae72815bb42c0452011183ce9c34093
Paja6544 <Pavankumar8011@gmail.com>
2025-05-07 12:30:58 +0545

    created 404 error page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+ get_header();
+?>
+<div class="section">
+    <div class="container">
+    <div class="page-not-found">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/404-error.webp" alt="">
+    <p>Page Not Found</p>
+    <p>The page you are looking for doesn’t exist or has been moved.</p>
+    <a href="/" class="btn-all">Go Home</a>
+</div>
+    </div>
+</div>
+
+<?php
+ get_footer();
+?>
